257d2046970457e62d4e596b20197f20cb0f7758
Vayver <60351332+Vayver@users.noreply.github.com>
2021-07-07 12:14:31 +0200

    First plugin

---
+++ app/code/ATCP/MageAcademy/Plugin/Model/ProductPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ATCP\MageAcademy\Plugin\Model;
+
+use \Magento\Catalog\Model\Product;
+
+class ProductPlugin
+{
+    public function afterGetName(Product $subject, $result): string
+    {
+        return "[$result]";
+    }
+}
