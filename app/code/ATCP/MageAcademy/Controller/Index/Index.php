5e5cfc5ff6d0f360327875e1614bd8e15fd98d7c
Vayver <60351332+Vayver@users.noreply.github.com>
2021-07-14 10:15:11 +0200

    First Controller

---
+++ app/code/ATCP/MageAcademy/Controller/Index/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ATCP\MageAcademy\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+
+class Index implements ActionInterface
+{
+    public function execute()
+    {
+        die('Example');
+        // TODO: Implement execute() method.
+    }
+}
