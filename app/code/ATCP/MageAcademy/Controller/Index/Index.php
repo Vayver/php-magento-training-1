37b6bb0091c2d6e0f090d2552225fb9f48433cc3
Vayver <60351332+Vayver@users.noreply.github.com>
2021-07-14 11:14:39 +0200

    Hello World Controller

--- app/code/ATCP/MageAcademy/Controller/Index/Index.php
+++ app/code/ATCP/MageAcademy/Controller/Index/Index.php
@@ -3,12 +3,22 @@
 namespace ATCP\MageAcademy\Controller\Index;
 
 use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\Result\JsonFactory;
 
 class Index implements ActionInterface
 {
+    private $jsonFactory;
+
+    /**
+     * Index constructor.
+     */
+    public function __construct(JsonFactory $jsonFactory)
+    {
+        $this->jsonFactory = $jsonFactory;
+    }
+
     public function execute()
     {
-        die('Example');
-        // TODO: Implement execute() method.
+        return $this->jsonFactory->create()->setJsonData('Hello world!');
     }
 }
