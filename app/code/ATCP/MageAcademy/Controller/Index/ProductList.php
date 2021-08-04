8677a9c5e516a59ec8e9cb6f4130b60224957640
Vayver <60351332+Vayver@users.noreply.github.com>
2021-08-04 14:38:43 +0200

    Product list controller with json output page

---
+++ app/code/ATCP/MageAcademy/Controller/Index/ProductList.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ATCP\MageAcademy\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+
+
+class ProductList implements ActionInterface
+{
+    private $jsonFactory;
+    private $product;
+    private $collectionFactory;
+
+    public function __construct(JsonFactory $jsonFactory, Product $product, CollectionFactory $collectionFactory)
+    {
+        $this->jsonFactory = $jsonFactory;
+        $this->product = $product;
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    public function getProductCollection()
+    {
+        $product_list = $this->collectionFactory->create();
+        $product_list->addAttributeToSelect('*');
+        $product_list->setPageSize(10);
+        return $product_list;
+    }
+
+    public function execute()
+    {
+        $to_display = array();
+        $list = $this->getProductCollection();
+        foreach ($list as $product) {
+            $product_id = $product->getId();
+            $product_name = $product->getName();
+            $to_display [] = array('id' => $product_id, 'name' => $product_name);
+        }
+        return $this->jsonFactory->create()->setJsonData(json_encode($to_display, JSON_PRETTY_PRINT));
+    }
+}
